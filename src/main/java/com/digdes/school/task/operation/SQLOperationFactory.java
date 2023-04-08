bf0e723439c118d48ba8a74900d6d890415c32bb
AleksandrKlimchuk <1vipe234@gmail.com>
2023-04-08 18:08:37 +0300

    Add factory implementation for operations

--- src/main/java/com/digdes/school/task/operation/SQLOperationFactory.java
+++ src/main/java/com/digdes/school/task/operation/SQLOperationFactory.java
@@ -1,7 +1,5 @@
 package com.digdes.school.task.operation;
 
-import jdk.dynalink.Operation;
-
 public interface SQLOperationFactory {
 
     SQLOperation createByRequest(String request) throws Exception;
